c88515b3af9a29d8ba9d8fec7814380ddb5c8add
dhakadabhishek00 <dhakadabhishek00@gmail.com>
2026-05-09 14:56:38 +0530

    Time: 47 ms (5.15%) | Memory: 18 MB (34.56%) - LeetSync

---
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+      void rotate(vector<vector<int>>& grid,int k,int p){
+               int n=grid.size();
+               int m=grid[0].size();
+               vector<int>arr;
+               for(int i=k;i<m-k;i++){
+                // cout<<grid[k][i]<<" ";
+                    arr.push_back(grid[k][i]);
+               }
+              //  cout<<"\n";
+               
+               for(int i=k+1;i<n-k;i++){
+                    // cout<<grid[i][m-k-1]<<" ";
+                    arr.push_back(grid[i][m-k-1]);
+               }
+                
+               for(int i=m-k-2;i>=k;i--){
+                    // cout<<grid[n-k-1][i]<<" ";
+                    arr.push_back(grid[n-k-1][i]);
+               }
+                
+               for(int i=n-k-2;i>k;i--){
+                    // cout<<grid[i][k]<<" ";
+                    arr.push_back(grid[i][k]);
+               }
+              
+              
+              int ns=arr.size();
+             
+             
+              int r=p % ns;
+              reverse(arr.begin(),arr.begin()+r);
+              reverse(arr.begin()+r,arr.end());
+              reverse(arr.begin(),arr.end());
+
+             
+              int idx=0;
+
+               for(int i=k;i<m-k;i++){
+                  grid[k][i]=arr[idx];
+                  idx++;
+               }
+               
+               for(int i=k+1;i<n-k;i++){
+                    grid[i][m-k-1]=arr[idx];
+                    idx++;
+               }
+                
+               for(int i=m-k-2;i>=k;i--){
+                    grid[n-k-1][i]=arr[idx];
+                    idx++;
+                    
+               }
+               
+               for(int i=n-k-2;i>k;i--){
+                    grid[i][k]=arr[idx]; 
+                    idx++;  
+               }
+
+      }
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+          int n=grid.size();
+          int m=grid[0].size();
+          int s=min(n,m);
+          for(int i=0;i<(s/2);i++)
+             rotate(grid,i,k);
+           for(int i=0;i<n;i++){
+               for(int j=0;j<m;j++){
+                  cout<<grid[i][j]<<" ";
+               }
+               cout<<"\n";
+           }
+           return grid;
+    }
+};
