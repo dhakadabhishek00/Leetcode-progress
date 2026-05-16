0b0d419795a26270645e91678b40ac958fa136f7
dhakadabhishek00 <dhakadabhishek00@gmail.com>
2026-05-16 06:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (90.00%) - LeetSync

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+            int ans=INT_MAX;
+            for(int a:nums){
+               ans=min(ans,a);
+            }
+            return ans;
+    }
+};
