f6420de1a31f9b06ea03256ac123cfb615fdfb33
ABHISHEK Dhakad <dhakadabhishek00@gmail.com>
2026-09-18 22:43:14 +0530

    Time: 10 ms (5.52%) | Memory: 30.2 MB (5.07%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,24 +1,48 @@
 class Solution {
 public:
+    string getdir(string path,int &i){
+           string ans="";
+           while(i<path.size() && path[i]!='/'){
+                 ans+=path[i];
+                 i++;
+           }
+        
+           return ans;
+
+    }
     string simplifyPath(string path) {
-        vector<string> stack;
-        stringstream ss(path);
-        string token;
-
-        while (getline(ss, token, '/')) {
-            if (token == "" || token == ".") {
-                continue;
-            } else if (token == "..") {
-                if (!stack.empty()) stack.pop_back();
-            } else {
-                stack.push_back(token);
-            }
-        }
-
-        string ans;
-        for (string &dir : stack) {
-            ans += "/" + dir;
-        }
-        return ans.empty() ? "/" : ans;
+         vector<string>store;
+         int i=0;
+         int n=path.size();
+         while(i<n){
+              string str=getdir(path,i);
+              if(str.size()==0 || str=="."){
+                  //nothing
+              }
+              else if(str==".." && store.size()>0){
+                  store.pop_back();   
+              }
+              
+              else if(str==".." && store.size()==0){
+                  //nothing
+              }
+              else{
+                 store.push_back(str);
+              }
+              
+              i++;
+         }
+        string ans="";
+        i=0;
+         while(i<store.size()){
+          //  ans="/"+store.top()+ans;
+           ans+="/"+store[i];
+           i++;
+         }
+         if(ans.size()==0){
+           ans+="/";
+         }
+        
+         return ans;
     }
 };
